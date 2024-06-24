904b7d65f1dca212afba80f133f1e79c6819ad48
j4ckrisz <frenshystyle255@protonmail.com>
2024-06-24 10:33:33 -0500

    coding program gui

--- file_check_tkinter_program/main.py
+++ file_check_tkinter_program/main.py
@@ -1,8 +1,25 @@
 import tkinter as tk
+from tkinter import ttk
 
-class File_interity_checker(tk.Tk):
 
-    def __init__(self, *args, **kwargs):
+class App(tk.Tk):
+  
+  def __init__(self):
+    super().__init__()
+    # Configuring title, icon and size
+    self.title('File Integrity Checker')
+    #self.iconbitmap('images/logo.png')
+    self.geometry('500x500')
 
-        tk.Tk.__init__(self, *args, **kwargs)
-        container = tk.Frame(self)
+# Widgets
+    # Labels
+    self.mylabel = ttk.Label(self, text='Write the file full path ', font=('Helvetica', 12))
+    self.mylabel.pack(pady=20)
+
+    # Buttons
+    self.mybutton = ttk.Button(self, text='Compare')
+    self.mybutton.pack(pady=20)
+
+if __name__ == "__main__":
+  app = App()
+  app.mainloop()
